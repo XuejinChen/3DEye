6df9993088407064aea9b2bbf86058c8986af90e
taotie144 <32126852+taotie144@users.noreply.github.com>
2018-05-16 18:35:21 +0800

    Update 3dtrace.h

--- 3dtrace/3dtrace.h
+++ 3dtrace/3dtrace.h
@@ -10,15 +10,15 @@
 /*
 所有眼球B超图片的大小均为720*576，把查找区域限定在以下的范围内
 */
-#define START_ROW 70
+#define START_ROW 60
 #define END_ROW 510
 #define START_COL 70
 #define END_COL 640
 #define MAXN 5000
 
 #define BEGIN 50 //代表轮廓前端的检测范围
-#define END 230 //代表轮廓后端的检测范围
-#define MAX_LENGTH 350 //代表轮廓长度的上限
+#define END 240 //代表轮廓后端的检测范围，若末端检测太短了可以适当向上调整
+#define MAX_LENGTH 400 //代表轮廓长度的上限
 #define BLUR Size(13, 13) //均值滤波的参数，两个整数最好保持一致，如果轮廓查找有问题可以将此参数适当地向上调整
 
 using namespace std;
